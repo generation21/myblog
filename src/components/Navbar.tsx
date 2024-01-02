371cef2ec4851907ec041ccba55cf84adf5bfb48
generation21 <qbration21@gmail.com>
2024-01-03 00:28:09 +0900

    [refactor] router -> Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,17 @@
-"use client";
 import Link from "next/link";
 import React from "react";
 import ThemeToggleButton from "../features/theme/ThemeToggleButton";
 import { CONFIG } from "src/config";
-import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-    const router = useRouter();
     return (
         <nav className="sticky top-0 flex items-center justify-between w-full h-6 px-4 py-8 mx-auto mb-2 z-99 max-w-7xl bg-opacity-60 backdrop-blur-sm ">
             <div className="flex flex-row items-center gap-3">
-                <p
-                    className="text-lg font-bold cursor-pointer hover:text-primary"
-                    onClick={() => {
-                        router.push("/");
-                    }}
-                >
-                    {CONFIG.profile.name}
-                </p>
+                <Link href={"/"}>
+                    <p className="text-lg font-bold cursor-pointer hover:text-primary">
+                        {CONFIG.profile.name}
+                    </p>
+                </Link>
                 <span className="text-sm text-text-primary dark:text-text-dark">
                     블로그
                 </span>
